a2d29137df46ab55551bd97aa5a85bfe03d09ba9
james <jpratt20@ischool.org>
2020-05-26 22:35:11 -1000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,3 @@
+import {
+    double
+} from "./musicPlayer.js";
---
+++ js/musicPlayer.js
@@ -0,0 +1,5 @@
+music_name = "song.mp4"
+
+let playBtn = document.querySelector("#play");
+let nextBtn = document.querySelector("#next");
+let prevBtn = document.querySelector("#prev")
